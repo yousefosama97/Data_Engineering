22b9416ce05f0a0d899bda755fc6213d1416a743
yousefosama97 <53444095+yousefosama97@users.noreply.github.com>
2023-06-03 00:14:31 +0300

    First_Dag.py

--- First_Dag.py
+++ First_Dag.py
@@ -8,7 +8,6 @@ from airflow.operators.bash_operator import BashOperator
 from airflow.utils.dates import days_ago
 
 #defining DAG arguments
-# You can override them on a per-task basis during operator initialization
 default_args = {
     'owner': 'Joe',
     'start_date': days_ago(0),
